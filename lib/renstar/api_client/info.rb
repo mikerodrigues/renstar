041e3992177075356d15981ddca6706de81421f0
Michael Rodrigues <mikebrodrigues@gmail.com>
2021-10-26 18:26:41 -0700

    who controls the past now controls the future

---
+++ lib/renstar/api_client/info.rb
@@ -0,0 +1,18 @@
+module Renstar
+  module APIClient
+    class Info
+      def initialize(info_hash)
+        @raw_hash = info_hash
+        info_hash.each do |key, value|
+          instance_variable_set("@" + key, value)
+        end
+      end
+
+      def to_s
+        @raw_hash.map do |key, value|
+          APIClient.lookup(:info, key, value)
+        end.join(",\n")
+      end
+    end
+  end
+end
